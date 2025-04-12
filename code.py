727c162db3c896218d4516561db0ffb2d2ac60a4
sakshisinha15 <149610214+sakshisinha15@users.noreply.github.com>
2025-04-12 10:03:07 +0530

    datacleaningcode.py

--- code.py
+++ code.py
@@ -22,3 +22,29 @@ print(df.info())
 print("------------------------------------\n")
 print("MEAN ,STD,QUARTILE,MIN,MAX OF THE DATASET")
 print("------------------------------------\n")
+#info about the mean,std.deviation,quartile one,two,three
+print(df.describe())
+print("------------------------------------------------------------\n")
+print("                    DATA  CLEANING                            ")
+print("------------------------------------------------------------\n")
+print(df.isnull().sum())
+#Filling missing values with specific values
+df_filled=df.fillna({'geologic_province':df['geologic_province'].mode()[0],'country': df['country'].mode()[0],
+                     'reference':df['reference'].mode()[0]})
+print("\nDataframe after filling missing values:")
+print(df_filled)
+print(df_filled.isnull().sum())
+
+#Dropping rows with missing values
+df_dropped_rows=df.dropna()
+print("\nDataFrame after dropping rows with missing values:")
+
+#Dropping columns with missing values
+df_dropped_cols=df.dropna(axis=1)
+print("\nDataFrame after dropping cols with missing values:")
+print(df_dropped_cols)
+
+#saving cleaned data
+cleaned_file_path="datasetproject.csv"
+df.to_csv(cleaned_file_path,index=False)
+print(f"Cleaned data saved to: {cleaned_file_path}")
